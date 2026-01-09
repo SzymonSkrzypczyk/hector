d06ecdc99eb97d8602a8ce90a4f1ac157e5f2fff
Szymon Skrzypczyk <Szymon.Skrzypczyk@vp.pl>
2026-01-09 20:32:10 +0100

    available themes command

--- cmd/themes.go
+++ cmd/themes.go
@@ -1,40 +1,42 @@
-/*
-Copyright © 2026 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
+	"os"
 )
 
-// themesCmd represents the themes command
+func ListDirectories(path string) ([]string, error) {
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var dirs []string
+	for _, entry := range entries {
+		if entry.IsDir() {
+			dirs = append(dirs, entry.Name())
+		}
+	}
+
+	return dirs, nil
+}
+
 var themesCmd = &cobra.Command{
 	Use:   "themes",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Get a list of available themes",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("themes called")
+		directory := "themes/"
+		themes, err := ListDirectories(directory)
+		if err != nil {
+			cmd.PrintErrln("Error:", err)
+		}
+		cmd.Println("Available themes:")
+		for ind, theme := range themes {
+			cmd.Printf("Theme %d: %s\n", ind+1, theme)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(themesCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// themesCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// themesCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
