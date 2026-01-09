bb0b12f15ad4937a3b7b885e2f2428d4ca84795d
Szymon Skrzypczyk <Szymon.Skrzypczyk@vp.pl>
2026-01-09 21:13:15 +0100

    photo addition beginning

--- internal/schemas/gray_schema.go
+++ internal/schemas/gray_schema.go
@@ -1,9 +1,12 @@
 package schemas
 
+import "html/template"
+
 type Gray struct {
 	CandidateInfo   GrayCandidateInfo `yaml:"candidate_info" json:"candidate_info"`
 	Contact         GrayContact       `yaml:"contact" json:"contact"`
 	Skills          []string          `yaml:"skills" json:"skills"`
+	Photo           template.URL      `yaml:"photo" json:"photo"`
 	Languages       []GrayLanguage    `yaml:"languages" json:"languages"`
 	Education       []GrayEducation   `yaml:"education" json:"education"`
 	Experience      []GrayExperience  `yaml:"experience" json:"experience"`
--- internal/schemas/normal_schema.go
+++ internal/schemas/normal_schema.go
@@ -1,9 +1,12 @@
 package schemas
 
+import "html/template"
+
 type Normal struct {
 	CandidateInfo   NormalCandidateInfo `yaml:"candidate_info" json:"candidate_info"`
 	Contact         NormalContact       `yaml:"contact" json:"contact"`
 	Skills          []string            `yaml:"skills" json:"skills"`
+	Photo           template.URL        `yaml:"photo" json:"photo"`
 	Languages       []NormalLanguage    `yaml:"languages" json:"languages"`
 	Education       []NormalEducation   `yaml:"education" json:"education"`
 	Experience      []NormalExperience  `yaml:"experience" json:"experience"`
