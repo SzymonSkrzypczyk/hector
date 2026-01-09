93509d90f50e84249c16b86a3bba8a86c06b7648
Szymon Skrzypczyk <Szymon.Skrzypczyk@vp.pl>
2026-01-09 20:50:56 +0100

    unnecessary file cut out + defined new template

--- internal/theme/select_theme.go
+++ internal/theme/select_theme.go
@@ -9,6 +9,8 @@ func SelectTheme(themeName string) schemas.ThemeSchema {
 	switch strings.TrimSpace(strings.ToLower(themeName)) {
 	case schemas.Normal{}.ThemeName():
 		return &schemas.Normal{}
+	case schemas.Gray{}.ThemeName():
+		return &schemas.Gray{}
 	default:
 		return &schemas.Normal{}
 	}
