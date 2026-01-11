7f2823c9e459e117bc52584674e8485d47d7c07e
Szymon Skrzypczyk <Szymon.Skrzypczyk@vp.pl>
2026-01-11 12:17:03 +0100

    define validate command

---
+++ cmd/validate.go
@@ -0,0 +1,49 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"log"
+)
+
+const defaultTemplateValidation = "normal"
+
+var (
+	validationDataFile string
+	validationTemplate string
+)
+
+// validateCmd represents the validate command
+var validateCmd = &cobra.Command{
+	Use:   "validate",
+	Short: "Validate the CV data file",
+	Run: func(cmd *cobra.Command, args []string) {
+		if validationDataFile == "" {
+			log.Fatalln("Error: --data flag is required")
+		}
+
+		if validationTemplate == "" {
+			validationTemplate = defaultTemplateValidation
+		}
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(validateCmd)
+
+	validateCmd.Flags().StringVarP(
+		&validationDataFile,
+		"data",
+		"d",
+		"",
+		"Data file to be validated",
+	)
+
+	validateCmd.Flags().StringVarP(
+		&validationTemplate,
+		"template",
+		"t",
+		"normal",
+		"Template to validate against",
+	)
+}
